553df8d75a2c8897d9ecf77cfedcbae69fac3e0d
NMID <nmid@NMIDdeMac-mini.local>
2017-03-18 19:39:11 +0800

    Initial Commit

---
+++ Young Eagles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Young Eagles
+//
+//  Created by NMID on 2017/3/18.
+//  Copyright © 2017年 NMID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Young Eagles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Young Eagles
+//
+//  Created by NMID on 2017/3/18.
+//  Copyright © 2017年 NMID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
